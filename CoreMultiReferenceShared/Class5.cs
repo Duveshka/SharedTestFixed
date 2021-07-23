5f02d259e5650b9d847632169c7ab9e50d0e54cd
duveshka <duveshka@mail.ru>
2021-07-23 13:02:00 +0300

    Add project files.

---
+++ CoreMultiReferenceShared/Class5.cs
@@ -0,0 +1,26 @@
+﻿#region test
+
+using System;
+using NewFolder4;
+using ReferencedMultipleShared;
+
+#endregion
+
+namespace CoreMultiReferenceShared
+{
+    public class Class5
+    {
+        public void Test()
+        {
+            var reSharper1 = new ReferencedMultipleShared_ReSharper1_Renamed();
+            var a = new A5();
+            a.Test();
+            MethodForTest(a);
+        }
+
+        public void MethodForTest (IInterface test)
+        {
+            test.Test();
+        }
+    }
+}
---
+++ ReferencedMultipleShared/NewFolder4/asdf.cs
@@ -0,0 +1,21 @@
+﻿#region test
+
+using System;
+
+#endregion
+
+     namespace NewFolder4   
+    {
+        public interface IInterface
+        {
+            void Test();
+        }
+        public class A5 : IInterface
+        {
+            public void Test()
+            {
+                int t = 1;
+                Console.WriteLine("Tadada");
+            }
+        }
+    }
