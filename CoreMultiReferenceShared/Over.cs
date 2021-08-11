aa48e8da76e065ef9550947cab2b50abb6146ec2
duveshka <duveshka@mail.ru>
2021-08-11 12:40:03 +0300

    Add for overriding methods

---
+++ CoreMultiReferenceShared/Over.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreMultiReferenceShared
+{
+    abstract class Shape
+    {
+        public abstract int GetArea();
+    }
+
+    class Square : Shape
+    {
+        int side;
+
+        public Square(int n) => side = n;
+
+        public override int GetArea() => side * side;
+    }
+}
