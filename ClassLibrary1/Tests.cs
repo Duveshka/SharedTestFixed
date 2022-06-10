3238a063011189662be9c9bd019dc69bdc49b032
Ekaterina Kuznetsova <Ekaterina.Kuznetsova@jetbrains.com>
2022-06-10 22:27:09 +0300

    add duplicate in hierarchy

---
+++ ClassLibrary1/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    internal class Tests
+    {
+    }
+
+
+    internal class Deactivate
+    {
+    }
+
+    class DeleteAsset
+    {
+        
+    }
+
+    class Details
+    {
+        
+    }
+
+    class Delete
+    {
+        
+    }
+}
---
+++ CoreMultiReferenceShared/Interface2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreMultiReferenceShared
+{
+    internal interface Interface2
+    {
+    }
+    internal interface Interface3 : Interface2
+    {
+    }
+
+    public class class_1 : Interface3
+    {
+
+    }
+    public class class_3 : class_1
+    {
+
+    }
+}
